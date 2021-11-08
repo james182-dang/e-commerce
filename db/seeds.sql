693d45b31a687336c9ff34c55c94e8edfd2067bd
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-11-08 12:47:06 -0600

    set up schema and seed files, set up models, begin set up on routes

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO category (name)
+VALUES
+('Shirts'),
+('Shorts'),
+('Music'),
+('Hats'),
+('Shoes');
+
+INSERT INTO tag (name)
+VALUES
+('rock_music'),
+('pop_music'),
+('blue'),
+('red'),
+('green'),
+('white'),
+('gold'),
+('pop_culture');
+
+INSERT INTO product (name, price, stock, category_id)
+VALUES
+('Plain T-Shirt', 14.99, 14, 1),
+('Running Sneakers', 90.0, 25, 5),
+('Branded Baseball Hat', 22.99, 12, 4),
+('Top 40 Music Compilation Vinyl Record', 12.99, 50, 3),
+('Cargo Shorts', 29.99, 22, 2);
